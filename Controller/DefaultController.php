2017ed9f211fad0aab9b89d2aaf8d6904620dd54
Nimbus Le Truand <nimbusletruand@gmail.com>
2014-01-19 00:36:26 +0100

    remove var_dump
    Signed-off-by: Nimbus Le Truand <nimbusletruand@gmail.com>

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -19,7 +19,6 @@ class DefaultController extends ContainerAware
         $this->Lang = (string) $_locale;
 
         $pathToFile = __DIR__.'/../../../'.$this->container->getParameter('nimbusletruand_curriculumvitae.path_to_cv').'/'.$this->FileToLoad.'.xml';
-        var_dump($pathToFile);
         if (!is_file($pathToFile)) {
             throw new NotFoundHttpException('There is no curriculum vitae file defined for '.$this->FileToLoad.'.');
         }
