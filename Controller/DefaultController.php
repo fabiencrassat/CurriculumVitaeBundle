1d34fb52ee6de3e8a8e040875e6ae6d83facbbaf
Fabien Crassat <fabien@crassat.com>
2014-03-29 17:54:53 +0100

    Added copyrights Signed-off-by: Fabien Crassat <fabien@crassat.com>

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Nimbusletruand\CurriculumVitaeBundle Symfony bundle.
+ *
+ * (c) Fabien Crassat <fabien@crassat.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Nimbusletruand\CurriculumVitaeBundle\Controller;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
