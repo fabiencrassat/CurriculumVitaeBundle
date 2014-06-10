1bcb883533755194453ed2c6e0b5a0b0ad3d728e
Fabien Crassat <fabien@crassat.com>
2014-06-10 02:32:32 +0200

    Installation de l'export PDF
    Signed-off-by: Fabien Crassat <fabien@crassat.com>

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -11,19 +11,78 @@
 
 namespace Nimbusletruand\CurriculumVitaeBundle\Controller;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Serializer\Exception\InvalidArgumentException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
 
 use Nimbusletruand\CurriculumVitaeBundle\Entity\CurriculumVitae;
 
-class DefaultController extends ContainerAware
+class DefaultController extends Controller
 {
     private $Lang;
     private $ReadCVXml;
     private $FileToLoad;
+    private $exposedLanguages;
 
     public function indexAction($cvxmlfile, $_locale)
+    {
+        $this->readCVFile($cvxmlfile, $_locale);
+
+        return $this->container->get('templating')->renderResponse(
+            $this->container->getParameter('nimbusletruand_curriculumvitae.template'), array(
+                'cvxmlfile'         => $this->FileToLoad,
+                'languageView'      => $this->Lang,
+                'languages'         => $this->exposedLanguages,
+                'anchors'           => $this->ReadCVXml->getAnchors(),
+                'identity'          => $this->ReadCVXml->getIdentity(),
+                'followMe'          => $this->ReadCVXml->getFollowMe(),
+                'lookingFor'        => $this->ReadCVXml->getLookingFor(),
+                'experiences'       => $this->ReadCVXml->getExperiences(),
+                'skills'            => $this->ReadCVXml->getSkills(),
+                'educations'        => $this->ReadCVXml->getEducations(),
+                'languageSkills'    => $this->ReadCVXml->getLanguageSkills(),
+                'miscellaneous'     => $this->ReadCVXml->getMiscellaneous(),
+                'society'           => $this->ReadCVXml->getSociety()
+        ));
+    }
+
+    public function exportPDFAction($cvxmlfile, $_locale)
+    {
+        $this->readCVFile($cvxmlfile, $_locale);
+
+        $html = $this->container->get('templating')->renderResponse(
+            $this->container->getParameter('nimbusletruand_curriculumvitae.template'), array(
+                'cvxmlfile'         => $this->FileToLoad,
+                'languageView'      => $this->Lang,
+                'languages'         => $this->exposedLanguages,
+                'anchors'           => $this->ReadCVXml->getAnchors(),
+                'identity'          => $this->ReadCVXml->getIdentity(),
+                'followMe'          => $this->ReadCVXml->getFollowMe(),
+                'lookingFor'        => $this->ReadCVXml->getLookingFor(),
+                'experiences'       => $this->ReadCVXml->getExperiences(),
+                'skills'            => $this->ReadCVXml->getSkills(),
+                'educations'        => $this->ReadCVXml->getEducations(),
+                'languageSkills'    => $this->ReadCVXml->getLanguageSkills(),
+                'miscellaneous'     => $this->ReadCVXml->getMiscellaneous(),
+                'society'           => $this->ReadCVXml->getSociety()
+        ));
+
+        $identity = $this->ReadCVXml->getIdentity();
+        $lookingFor = $this->ReadCVXml->getLookingFor();
+        $filename = $identity['myself']['Name'].' - '.$lookingFor['experience']['job'].'.pdf';
+
+        return new Response(
+            $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'attachment; filename="'.$filename.'"'
+            )
+        );
+    }
+
+    private function readCVFile($cvxmlfile, $_locale)
     {
         if ($this->container->getParameter('nimbusletruand_curriculumvitae.default_cv') == $cvxmlfile) {
             $custo_default_cv = $this->container->getParameter('nimbusletruand_curriculumvitae.custo_default_cv');
@@ -41,25 +100,9 @@ class DefaultController extends ContainerAware
 
         $this->ReadCVXml = new CurriculumVitae($pathToFile, $this->Lang);
 
-        $exposedLanguages = $this->ReadCVXml->getDropDownLanguages();
-        if (!array_key_exists($_locale, $exposedLanguages)) {
+        $this->exposedLanguages = $this->ReadCVXml->getDropDownLanguages();
+        if (!array_key_exists($_locale, $this->exposedLanguages)) {
             throw new NotFoundHttpException('There is no curriculum vitae defined for this language');
-        }
-
-        return $this->container->get('templating')->renderResponse(
-            $this->container->getParameter('nimbusletruand_curriculumvitae.template'), array(
-                'cvxmlfile'         => $this->FileToLoad,
-                'languages'         => $exposedLanguages,
-                'anchors'           => $this->ReadCVXml->getAnchors(),
-                'identity'          => $this->ReadCVXml->getIdentity(),
-                'followMe'          => $this->ReadCVXml->getFollowMe(),
-                'lookingFor'        => $this->ReadCVXml->getLookingFor(),
-                'experiences'       => $this->ReadCVXml->getExperiences(),
-                'skills'            => $this->ReadCVXml->getSkills(),
-                'educations'        => $this->ReadCVXml->getEducations(),
-                'languageSkills'    => $this->ReadCVXml->getLanguageSkills(),
-                'miscellaneous'     => $this->ReadCVXml->getMiscellaneous(),
-                'society'           => $this->ReadCVXml->getSociety()
-        ));
+        };
     }
 }
