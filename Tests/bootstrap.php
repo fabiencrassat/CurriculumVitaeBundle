238425a0b09ebe4bc83576a9d61e5c73c1c027d0
Fabien Crassat <fabien@crassat.com>
2014-06-29 17:16:24 +0200

    Fix phpunit for travais ci
    Signed-off-by: Fabien Crassat <fabien@crassat.com>

---
+++ Tests/bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Nimbusletruand\CurriculumVitaeBundle Symfony bundle.
+ *
+ * (c) Fabien Crassat <fabien@crassat.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$autoloads = array(
+    __DIR__.'/../vendor/autoload.php',                // travis or standalone test
+    __DIR__.'/../../../../../../vendor/autoload.php', // symfony & composer test
+);
+
+$autoloadFile = false;
+
+foreach ($autoloads as $file) {
+    if (is_file($file)) {
+        $autoloadFile = $file;
+
+        break;
+    }
+}
+
+if (!$autoloadFile) {
+    die('Unable to find autoload.php file, please use composer to load dependencies:
+
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+
+Visit http://getcomposer.org/ for more information.
+
+');
+}
+
+include $autoloadFile;
