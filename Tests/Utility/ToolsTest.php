471e2e4bf2613b93d9d790042f078808047ddc38
Fabien Crassat <fabien@crassat.com>
2014-11-09 00:51:10 +0100

    Revert "Increase code coverage"
    This reverts commit 55e8ab4afb94f95ebbb0c23111150ecfe6580cef.

--- Tests/Utility/ToolsTest.php
+++ Tests/Utility/ToolsTest.php
@@ -18,192 +18,39 @@ class ToolsTest extends \PHPUnit_Framework_TestCase
 
     private $tools;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->tools = new Tools();
     }
 
-    public function testArraysAreSimilar() {
+    public function testArraysAreSimilar()
+    {
         $this->assertEquals(0, $this->tools->arraysAreSimilar(array(), array()));
     }
 
-    public function testArraysAreNotSimilar() {
+    public function testArraysAreNotSimilar()
+    {
         $this->assertNotEquals(0, $this->tools->arraysAreSimilar(array("a"), array("b")));
     }
 
-    public function testArraysAreNotSimilarWithArray() {
+    public function testArraysAreNotSimilarWithArray()
+    {
         $this->assertNotEquals(0, $this->tools->arraysAreSimilar(array('a' => array("a")), array()));
     }
 
-    public function testArraysAreNotSimilarWithNotArray() {
+    public function testArraysAreNotSimilarWithNotArray()
+    {
         $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array("a")),
+            array('a' => array("a")), 
             array('a' => "a")
         ));
     }
 
-    public function testArraysAreNotSimilarWithArrayInArray0() {
+    public function testArraysAreNotSimilarWithArrayInArray()
+    {
         $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array('b' => array('c'))),
+            array('a' => array('b' => array('c'))), 
             array('a' => array('b' => array('d')))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray1() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array(
-                'b' => array('1'),
-                'c' => array('2'),
-                'd' => array('3'),
-                'e' => array('4'),
-                'f' => array('5'),
-                'g' => array('6'),
-                'h' => array('7'),
-                'i' => array('8'),
-                'j' => array('9'),
-            )),
-            array('a' => array(
-                'b' => array('0'),
-            ))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray2() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array(
-                'b' => array('0'),
-            )),
-            array('a' => array(
-                'b' => array('1'),
-                'c' => array('2'),
-                'd' => array('3'),
-                'e' => array('4'),
-                'f' => array('5'),
-                'g' => array('6'),
-                'h' => array('7'),
-                'i' => array('8'),
-                'j' => array('9'),
-            ))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray3() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array(
-                'b' => array('c' => array('0')),
-            )),
-            array('a' => array(
-                'b' => array('c' => array('1'))
-            ))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray4() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array('b' => array(
-                'c' => array('d' => array('0'))
-            ))),
-            array('a' => array('b' => array(
-                'c' => array('d' => array('1'))
-            )))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray5() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array('b' => array('c' => array(
-                'd' => array('0'),
-                'e' => array('0')
-            )))),
-            array('a' => array('b' => array('c' => array(
-                'd' => array('1'),
-                'e' => array('1')
-            ))))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray6() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array('b' => array('c' => array(
-                'd' => array('0'),
-                'e' => array('f' => array('0'))
-            )))),
-            array('a' => array('b' => array('c' => array(
-                'd' => array('1'),
-                'e' => array('f' => array('1'))
-            ))))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray7() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array('b' => array('c' => array(
-                'd' => array('0'),
-                'e' => array(
-                    'f' => array('0'),
-                    'g' => array('0')
-                )
-            )))),
-            array('a' => array('b' => array('c' => array(
-                'd' => array('1'),
-                'e' => array(
-                    'f' => array('1'),
-                    'g' => array('1')
-                )
-            ))))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray8() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array('b' => array('c' => array(
-                'd' => array('0'),
-                'e' => array(
-                    'f' => array('0'),
-                    'g' => array(
-                        'h' => 0,
-                        1,
-                        2
-                    )
-                )
-            )))),
-            array('a' => array('b' => array('c' => array(
-                'd' => array('1'),
-                'e' => array(
-                    'f' => array('1'),
-                    'g' => array(
-                        'h' => 1,
-                        2,
-                        3
-                    )
-                )
-            ))))
-        ));
-    }
-
-    public function testArraysAreNotSimilarWithArrayInArray9() {
-        $this->assertNotEquals(0, $this->tools->arraysAreSimilar(
-            array('a' => array('b' => array('c' => array(
-                'd' => array('0'),
-                'e' => array(
-                    'f' => array('0'),
-                    'g' => array(
-                        'h' => 0,
-                        1,
-                        2
-                    )
-                )
-            )))),
-            array('a' => array('b' => array('c' => array(
-                'd' => array('1'),
-                'e' => array(
-                    'f' => array('1'),
-                    'g' => array(
-                        'h' => 1,
-                        2,
-                        array()
-                    )
-                )
-            ))))
         ));
     }
 }
