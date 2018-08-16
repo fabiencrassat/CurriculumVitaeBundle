d5bbd6a29365a936ca90a458ed3741e5847230ef
Fabien Crassat <fabien@crassat.com>
2018-08-16 22:31:24 +0200

    Prepare symfony 4 (continue...)
    Signed-off-by: Fabien Crassat <fabien@crassat.com>

---
+++ Tests/Fixtures/App/config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FabienCrassat\CurriculumVitaeBundle\FabienCrassatCurriculumVitaeBundle::class => ['all' => true],
+];
